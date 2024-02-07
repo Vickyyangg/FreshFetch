7be21248261ef2329be173c582a6425a1ec28776
NayHtetKyaw <nayhtetkyaw.dev@gmail.com>
2024-02-08 02:21:10 +0700

    update

---
+++ app/src/main/java/com/example/freshfetch/signup.kt
@@ -0,0 +1,16 @@
+package com.example.freshfetch
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class SingUp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
