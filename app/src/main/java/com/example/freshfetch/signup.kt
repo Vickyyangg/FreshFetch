d50f7b74efb85e1b906ee0d9d735d91fa3f64ceb
NayHtetKyaw <nayhtetkyaw.dev@gmail.com>
2024-02-08 03:00:02 +0700

    alignments

--- app/src/main/java/com/example/freshfetch/signup.kt
+++ app/src/main/java/com/example/freshfetch/signup.kt
@@ -1,16 +1,11 @@
 package com.example.freshfetch
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
 
-class SingUp : AppCompatActivity() {
+class SignUp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_signup)
     }
 }
