e8c5cb9e6e9c011ae2187b7ac77b2178c4a42daa
NayHtetKyaw <nayhtetkyaw.dev@gmail.com>
2024-02-08 17:47:42 +0700

    fixes

--- app/src/main/java/com/example/freshfetch/MainActivity.kt
+++ app/src/main/java/com/example/freshfetch/MainActivity.kt
@@ -5,16 +5,16 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.example.freshfetch.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val loginButton: Button = findViewById(R.id.loginButton)
-        val createAccountButton: Button = findViewById(R.id.createAccountButton)
-
-        loginButton.setOnClickListener(object : View.OnClickListener {
+        binding.loginButton.setOnClickListener(object : View.OnClickListener {
             override fun onClick(view: View) {
                 // Start the LoginActivity when the login button is clicked
                 val loginIntent = Intent(this@MainActivity, Login::class.java)
@@ -22,7 +22,7 @@ class MainActivity : AppCompatActivity() {
             }
         })
 
-        createAccountButton.setOnClickListener(object : View.OnClickListener {
+        binding.createAccountButton.setOnClickListener(object : View.OnClickListener {
             override fun onClick(view: View) {
                 // Start the SignupActivity when the create account button is clicked
                 val signupIntent = Intent(this@MainActivity, SignUp::class.java)
